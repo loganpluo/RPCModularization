cbf8f078d236a5fe4057c97e01e5d99afcdf0259
loganpluo <990740109@qq.com>
2020-04-18 21:16:08 +0800

    biz_module_api-module_topic_api

--- app/src/main/java/com/github/rpc/modularization/MyApplication.java
+++ app/src/main/java/com/github/rpc/modularization/MyApplication.java
@@ -8,7 +8,7 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
 
-        //todo 自动实例化， 模块有必要提供注册和反注册吗?
+        //todo 自动实例化， 模块有必要提供注册和反注册吗? 或者直接把 RPCModule合并到 RPCModuleService
         RPCModuleHelper.newInstanceAndInit(getApplicationContext(),"com.github.rpc.module_login.LoginModel");
 
     }
