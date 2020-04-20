c9a72c81d2d32d22b04ef060106d07f313ba7a65
loganpluo <990740109@qq.com>
2020-04-21 00:57:59 +0800

    debug 模块 支持 addModules

--- plugin-modularization/src/main/groovy/com/github/rpc/modularization/plugin/ModuleDebugAbleHelper.groovy
+++ plugin-modularization/src/main/groovy/com/github/rpc/modularization/plugin/ModuleDebugAbleHelper.groovy
@@ -20,7 +20,9 @@ class ModuleDebugAbleHelper {
         if(isMainApp(project)){//主app工程一直以application运行
             project.apply plugin: 'com.android.application'
         }else{
-            configDebugModelGradle(project)
+            boolean isDebugAlone = Boolean.parseBoolean((project.properties.get("isDebugAlone")))
+            configDebugModelGradle(project,isDebugAlone)
+            addModuleForDebugModel(project,isDebugAlone)
         }
 
         addModuleDependencyMethod(project)
@@ -68,8 +70,8 @@ class ModuleDebugAbleHelper {
      * @param mainModuleName
      * @return
      */
-    static boolean isMainAppBuild(String mainModuleName){
-        return mainModuleName == mainAppName
+    static boolean isMainAppBuild(String mainTaskName){
+        return mainTaskName == mainAppName
     }
 
     /**
@@ -77,9 +79,7 @@ class ModuleDebugAbleHelper {
      * config阶段 library模块 必须是 com.android.application工程，才会出现在 as的 可以run的选项
      * @param project
      */
-    static void configDebugModelGradle(Project project){
-
-        boolean isDebugAlone = Boolean.parseBoolean((project.properties.get("isDebugAlone")))
+    static void configDebugModelGradle(Project project, Boolean isDebugAlone){
 
         boolean isMainAppBuild = isMainAppBuild(mainTaskName)
         println "mainModuleName:$mainTaskName, isMainAppBuild:$isMainAppBuild"
@@ -130,7 +130,7 @@ class ModuleDebugAbleHelper {
     }
 
     /**
-     * addModule 动态依赖 业务模块支持， 注意只用在主工程依赖 业务实现模块
+     * gradle配置支持 addModule关键字，实现 动态依赖 业务模块支持， 注意只用在主工程依赖 业务实现模块
      *
      * @param project
      */
@@ -142,7 +142,7 @@ class ModuleDebugAbleHelper {
             if(!isMainAppBuild){//只要是主工程编译任务则 添加 实现模块
                 return
             }
-            println "${dependencyName} "
+            println "project.dependencies.add ${dependencyName} "
             def componentProject = project.rootProject.subprojects.find { it.name == dependencyName }
             def dependencyMode = GradleVersion.version(project.gradle.gradleVersion) >= GradleVersion.version('4.1') ? 'api' : 'compile'
             if (realDependency) {// todo 这个没理解？
@@ -165,6 +165,50 @@ class ModuleDebugAbleHelper {
                                 "\nelse" +
                                 "\nyou can specify the real dependency via add the 2nd param, for example: " +
                                 "addModule '$dependencyName', 'com.billy.demo:demoB:1.1.0'")
+            }
+        }
+
+    }
+
+    /**
+     *  独立debug 运行模块 通过gradle.properties的addModules配置 依赖的业务实现模块
+     * @param project
+     */
+    static void addModuleForDebugModel(Project project,Boolean isDebugAlone){
+        boolean isModuleDebugRun = isDebugAlone && (mainTaskName == project.name)
+        println "addModuleForDebugModel " +
+                "isModuleDebugRun:$isModuleDebugRun, " +
+                "isDebugAlone:$isDebugAlone, mainTaskName:$mainTaskName, project.name:${project.name}"
+        if(!isModuleDebugRun) return
+
+        String modules = (String)project.properties.get("addModules")
+
+        if (modules == null || modules.length() == 0) {
+            System.out.println("there is no add dependencies ")
+            return
+        }
+        String[] compileModules = modules.split(",")
+        if (compileModules == null || compileModules.length == 0) {
+            System.out.println("there is no add dependencies ")
+            return
+        }
+        for (String module : compileModules) {
+            System.out.println("comp is " + module)
+            if (module.contains(":")) {
+                /**
+                 * 示例语法:groupId:artifactId:version(@aar)
+                 * compileComponent=com.luojilab.reader:readercomponent:1.0.0
+                 * 注意，前提是已经将组件aar文件发布到maven上，并配置了相应的repositories
+                 */
+                project.dependencies.add("compile", module)
+                System.out.println("add dependencies lib  : " + module)
+            } else {
+                /**
+                 * 示例语法:module
+                 * compileComponent=readercomponent,sharecomponent
+                 */
+                project.dependencies.add("compile", project.project(':' + module))
+                System.out.println("add dependencies project : " + module)
             }
         }
 
