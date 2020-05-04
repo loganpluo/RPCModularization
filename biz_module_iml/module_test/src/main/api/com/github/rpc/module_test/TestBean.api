d746f0eaf4c6f3c1671080b681ec3940502488bc
loganpluo <990740109@qq.com>
2020-05-04 22:45:31 +0800

    自动构建api工程脚本；

---
+++ biz_module_iml/module_test/src/main/api/com/github/rpc/module_test/TestBean.api
@@ -0,0 +1,8 @@
+package com.github.rpc.module_test;
+
+public class TestBean{
+    public String name;
+    public TestBean(String name){
+        this.name = name;
+    }
+}
--- biz_module_iml/module_test/src/main/java/com/github/rpc/module_test/TestModuleServiceImpl.java
+++ biz_module_iml/module_test/src/main/java/com/github/rpc/module_test/TestModuleServiceImpl.java
@@ -4,8 +4,12 @@ import com.github.rpc.modularization.ModuleService;
 import com.github.rpc.module_login_api.LoginModuleService;
 import com.github.rpc.module_login_api.Session;
 
-//@ModuleService
-public class TestModuleServiceImpl  {//implements TestModuleService
+@ModuleService
+public class TestModuleServiceImpl implements TestModuleService  {
+    @Override
+    public TestBean getBean() {
+        return new TestBean("TestBean");
+    }//
 //    @Override
 //    public boolean isLogin() {
 //        return true;
